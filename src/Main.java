6e002382f01a9b8e6edea3fa340208e1a1b4771c
ShreyaGanesh1 <shreyag161206@gmail.com>
2026-04-11 21:27:50 +0530

    UC16: Sort Passenger Bogies by Capacity

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class CargoSafetyException extends RuntimeException {
     public CargoSafetyException(String message) {
         super(message);
@@ -59,5 +60,46 @@ public class Main {
         bogie2.assignCargo("Petroleum");
 
         System.out.println("\nUC15 runtime handling completed...");
+=======
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        System.out.println("=========================================");
+        System.out.println("UC16 - Manual Sorting using Bubble Sort");
+        System.out.println("=========================================\n");
+
+        System.out.println("Original Capacities:");
+        printArray(capacities);
+
+        bubbleSort(capacities);
+
+        System.out.println("\nSorted Capacities (Ascending):");
+        printArray(capacities);
+
+        System.out.println("\nUC16 sorting completed...");
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+>>>>>>> 37472a4 (UC16: Sort Passenger Bogies by Capacity)
     }
 }
